d663173c29c98578bd16a257ddd5cf485ab5a60c
hectorzaragoza <zaragoza09@gmail.com>
2021-11-19 11:14:54 -0600

    successfully created a comment. Ready to set up displaying comments

--- controllers/comment.js
+++ controllers/comment.js
@@ -4,7 +4,8 @@ const router = express.Router()
 
 router.get('/:id', (req, res) => {
     db.marker.findAll({
-        where: {id: req.params.id}
+        where: {id: req.params.id},
+        include: [db.comment]
     })
     .then(placeDetail => {
         res.render('comment', {placeDetail})
@@ -14,13 +15,21 @@ router.get('/:id', (req, res) => {
 router.post('/:id/comments', (req, res) => {
     let formName = req.body.name
     let formComment = req.body.comment
+    console.log('This is the id for this place: ', req.params.id)
     db.comment.create({
         name: formName,
-        comment: formComment    
+        comment: formComment,
+        userId: req.params.id,
+        markerId: req.params.id    
     })
     .then(createdComment => {
         console.log ('This is the created content: ', createdComment)
-        res.render('comment')
+        db.marker.findAll({
+            where: {id: req.params.id}
+        })
+        .then(placeDetail => {
+            res.render('comment', {placeDetail, createdComment})
+        })
     })
 })
 
